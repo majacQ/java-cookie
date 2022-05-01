dcef17686ed5eabeb9d931f9317be5671e13182e
Jacquelyn Vinson <jacqvinson@gmail.com>
2022-04-30 19:47:49 -0700

    Fix: decoding two encoded chars (#7)
    * Update Maven/NPM depdendencies, remove Grunt (not necessary)
    
    * Add failing test
    
    * Fix decoding by doing two rounds: first 3-byte-chars, then 1-byte-chars
    
    * Fix Java JDK versions for Travis
    
    * Remove project Eclipse files and ignore them
    
    * Fix scope of JBOSS dependency
    
    Co-authored-by: Thomas Lutz <lutz@symptoma.com>

--- src/test/java/com/github/jscookie/javacookie/test/unit/CookiesDecodingTest.java
+++ src/test/java/com/github/jscookie/javacookie/test/unit/CookiesDecodingTest.java
@@ -1,5 +1,7 @@
 package com.github.jscookie.javacookie.test.unit;
 
+import com.github.jscookie.javacookie.Cookies;
+import com.github.jscookie.javacookie.test.unit.utils.BaseTest;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,26 +9,40 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import com.github.jscookie.javacookie.Cookies;
-import com.github.jscookie.javacookie.test.unit.utils.BaseTest;
-
-@RunWith( MockitoJUnitRunner.class )
+@RunWith(MockitoJUnitRunner.class)
 public class CookiesDecodingTest extends BaseTest {
-	private Cookies cookies;
+  private Cookies cookies;
 
-	@Before
-	public void before() {
-		cookies = Cookies.initFromServlet( request, response );
-	}
+  @Before
+  public void before() {
+    cookies = Cookies.initFromServlet(request, response);
+  }
 
-	@Test
-	public void character_not_allowed_in_name_and_value() {
-		Mockito.when( request.getHeader( "cookie" ) ).thenReturn( "%3B=%3B" );
-		String actual = cookies.get( ";" );
-		String expected = ";";
-		Assert.assertEquals( expected, actual );
-	}
+  @Test
+  public void character_not_allowed_in_name_and_value() {
+    Mockito.when(request.getHeader("cookie")).thenReturn("%3B=%3B");
+    String actual = cookies.get(";");
+    String expected = ";";
+    Assert.assertEquals(expected, actual);
+  }
 
+  @Test
+  public void character_with_3_bytes() {
+    Mockito.when(request.getHeader("cookie")).thenReturn("c=%E4%BA%AC");
+    String actual = cookies.get("c");
+    String expected = "京";
+    Assert.assertEquals(expected, actual);
+  }
+
+  <<<<<<< fix/decoding-two-encoded-chars
+  @Test
+  public void two_encoded_characters() {
+    Mockito.when(request.getHeader("cookie")).thenReturn("c=New%20York%2C%20NY");
+    String actual = cookies.get("c");
+    String expected = "New York, NY";
+    Assert.assertEquals(expected, actual);
+  }
+  =======
 	@Test
 	public void character_with_2_bytes() {
 		Mockito.when( request.getHeader( "cookie" ) ).thenReturn( "c=%C3%A3" );
@@ -50,4 +66,6 @@ public class CookiesDecodingTest extends BaseTest {
 		String expected = "New York, NY";
 		Assert.assertEquals(expected, actual);
 	}
+  >>>>>>> main
 }
+ 
